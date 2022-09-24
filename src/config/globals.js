f8666ef6199a8fd24a96d50018c71d591425c269
unknown <scottymitch88@gmail.com>
2022-09-24 01:36:56 -0400

    Fix port

--- src/config/globals.js
+++ src/config/globals.js
@@ -6,7 +6,7 @@ const debug = process.env.NODE_ENV !== 'production'
 export default {
   app: {
     name: 'Proof Generation API',
-    port: parseInt(process.env.port || 5000, 10),
+    port: parseInt(process.env.PORT || 5000, 10),
     maticRPC: [process.env.MATIC_PROVIDER_URL],
     ethereumRPC: [process.env.ETHEREUM_PROVIDER_URL]
   },
