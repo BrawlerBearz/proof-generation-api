fa153d4db2b61fff084f2213de4330c5b6b2a9f7
unknown <scottymitch88@gmail.com>
2023-09-20 21:54:49 -0400

    Fix

--- src/config/globals.js
+++ src/config/globals.js
@@ -3,14 +3,15 @@ import { config } from 'dotenv'
 config()
 
 const debug = process.env.NODE_ENV !== 'production'
+
 export default {
   app: {
-    name: process.env.NAME || 'Proof Generation API',
+    name: 'Proof Generation API',
     port: parseInt(process.env.PORT || 5000, 10),
-    maticRPC: JSON.parse(process.env.MATIC_RPC),
-    ethereumRPC: JSON.parse(process.env.ETHEREUM_RPC),
-    goerliRPC: JSON.parse(process.env.GOERLI_RPC),
-    mumbaiRPC: JSON.parse(process.env.MUMBAI_RPC),
+    maticRPC: [process.env.MATIC_PROVIDER_URL],
+    ethereumRPC: [process.env.ETHEREUM_PROVIDER_URL],
+    mumbaiRPC: [process.env.MUMBAI_PROVIDER_URL],
+    goerliRPC: [process.env.GOERLI_PROVIDER_URL],
     zkEVMMainnetURL: process.env.ZKEVM_MAINNET_URL,
     zkEVMTestnetURL: process.env.ZKEVM_TESTNET_URL
   },
